e3992186466006feed59d4d99246de202dfea8ce
helee-geek <heleergeek435@gmail.com>
2025-12-24 14:19:21 +0530

    Add CORS middleware to enable cross-origin requests

--- app.js
+++ app.js
@@ -30,15 +30,23 @@
 const express = require('express');
 const passport = require('./config/passport');
 const authRoutes = require('./routes/authRoutes');
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 // Routes
 app.use('/api/auth', authRoutes);
+
 
 // Home route
 app.get('/login', (req, res) => {
